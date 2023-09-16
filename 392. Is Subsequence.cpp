965b2118f0e4993a3d5a8da75e20659095062e7a
Ankit Kumar <125347399+coolakt@users.noreply.github.com>
2023-09-17 00:57:03 +0530

    Update 392. Is Subsequence.cpp

--- 392. Is Subsequence.cpp
+++ 392. Is Subsequence.cpp
@@ -1,1 +1,23 @@
-jsj
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+         int subsize=s.size();
+          int tarsize=0;
+          int l=-1;
+        for(int i=0; i<s.size(); i++){
+            for(int j=l+1; j<t.size(); j++){
+                if(s[i] == t[j]){
+                    tarsize++;
+                    l=j;
+                    break;
+                }
+            }
+           
+        }
+       
+         if(subsize == tarsize){
+                return 1;
+            }
+        return 0;
+    }
+};
