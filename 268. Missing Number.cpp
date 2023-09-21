62eea4fe49cbfc2c9fb46b4e5a29f44522cccb64
Ankit Kumar <125347399+coolakt@users.noreply.github.com>
2023-09-22 00:17:24 +0530

    Update 268. Missing Number.cpp

--- 268. Missing Number.cpp
+++ 268. Missing Number.cpp
@@ -1,14 +1,26 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         int expectedSum = n*(n+1)/2;
+//         int sum=0;
+
+//         for(int i=0; i<n; i++)
+//             sum += nums[i];
+        
+//         return expectedSum - sum;
+
+//     }
+// };
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int expectedSum = n*(n+1)/2;
-        int sum=0;
-
-        for(int i=0; i<n; i++)
-            sum += nums[i];
-        
-        return expectedSum - sum;
-
+        int n=nums.size();
+        int ans=0;
+        for(int i=0; i<n; i++){
+            ans^=(nums[i]^i);
+        }
+        ans^=(n);
+        return ans;
     }
 };
