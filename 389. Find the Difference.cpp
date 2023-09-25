d87415750f56ff63b7a262fcc4437270a7f9ed86
Ankit Kumar <125347399+coolakt@users.noreply.github.com>
2023-09-26 00:34:03 +0530

    Update 389. Find the Difference.cpp

--- 389. Find the Difference.cpp
+++ 389. Find the Difference.cpp
@@ -1,1 +1,25 @@
-hh
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> count(26, 0); // Initialize a count array for characters 'a' to 'z'
+
+        // Count characters in string s
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+
+        // Subtract characters in string t
+        for (char c : t) {
+            count[c - 'a']--;
+        }
+
+        // Find the character with a count of -1 in t
+        for (int i = 0; i < 26; i++) {
+            if (count[i] == -1) {
+                return 'a' + i;
+            }
+        }
+
+        return ' '; // This line should not be reached
+    }
+};
